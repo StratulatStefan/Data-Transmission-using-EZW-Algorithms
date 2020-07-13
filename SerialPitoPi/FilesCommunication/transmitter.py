4a2f821b61caa9424468dc5b37879a8b876138f0
StratulatStefan <stefanc.stratulat@gmail.com>
2020-07-13 08:12:34 +0100

    serial-pi-to-pi-files-modification

--- SerialPitoPi/FilesCommunication/transmitter.py
+++ SerialPitoPi/FilesCommunication/transmitter.py
@@ -2,7 +2,7 @@ import serial
 import time
 import sys
 
-# definim portul serial pentru transmiterea datelor
+# definim si initializam portul serial pentru transmiterea datelor
 port = serial.Serial("/dev/ttyS0", baudrate=576000)
 
 # functia care citeste un fisier (in cazul nostru, o imagine) si ii returneaza
@@ -15,7 +15,6 @@ def ImageBinaryRead(filename):
 	# - asadar, trimitea fisierului se face secvential, prin descompunerea fisierului
 	# in cate block_size pachete si trimiterea fiecaruia pe rand
 	block_size = 512
-#	data = bytearray()
 	with open(filename, "rb") as file:
 		while True:
 			bytes = file.read(block_size)
@@ -29,37 +28,36 @@ data_encode = lambda message : message.encode("utf-8")
 if __name__ == "__main__":
 	print("----------------------------------------")
 
-	# numele imaginii va fi furnizat ca argument la linia de comanda
+	# numele fisierului va fi furnizat ca argument la linia de comanda
 	# asadar, verificam sa avem numarul corect de argumente furnizate
-	# avem nevoie 2 argumente (numele scriptului si numele imaginii)
+	# avem nevoie 2 argumente (numele scriptului si numele fisierului)
 	# daca numarul nu este corect, oprim executia programului
 	arguments_count = len(sys.argv)
 	if arguments_count != 2:
-		print("Furnizati numele imaginii!")
+		print("Furnizati numele fisierului!")
 		exit(-1)
 	image_filename = sys.argv[1]
-	print(f"Numele imaginii : {image_filename}")
+	print(f"Numele fisierului : {image_filename}")
 
 	'''
-	-  pentru ca receiver-ul sa poata scrie imaginea pe disc, acesta are nevoie 
-	de continutul imaginii si de numele acesteia; chiar daca numele imaginii este
-	codificat in headerele corespunzatoare imaginii, nu ne complicam sa il cautam 
-	acum, ci vom trimite mai intai numele imaginii, si apoi continutul
-	[nume] <nume_imagine>.<extensie>
-	[continut] <continut>
+	-  pentru ca receiver-ul sa poata scrie fisierul pe disc, acesta are nevoie 
+	de continutul fisierului si de numele acesteia; chiar daca numele fisierului este
+	codificat in headerele corespunzatoare fisierului, nu ne complicam sa il cautam 
+	acum, ci vom trimite mai intai numele fisierului, si apoi continutul
+	[nume] <nume_fisier>.<extensie>
+	<continut>
 	'''
 
-	# trimitem numele imaginii pe portul serial
+	# trimitem numele fisierului pe portul serial
 	encoded_filename = data_encode("[nume] " + image_filename)
 	bytes_count = port.write(encoded_filename)
 	print(f"[Am trimis \"{image_filename}\" [{bytes_count} octeti] pe portul serial!]")
-	time.sleep(0.1)
+	time.sleep(0.25)
 
 	# citim imaginea in format binar si trimitem continutul pe portul serial
-	#imageBitStream = ImageBinaryRead(image_filename)
 	for imageBitStream in ImageBinaryRead(image_filename):
 		bytes_count = port.write(imageBitStream)
 		print(f"[Am trimis {bytes_count} octeti pe portul serial!]")
-#		time.sleep(0.025)
 	print("\n")
-	print(f"[Imaginea a fost trimisa cu succes!]")
+	print(f"[Fisierul a fost trimis cu succes!]")
+
