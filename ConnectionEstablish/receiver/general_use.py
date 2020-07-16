e7a02aec05d819b7be18b440781ca7dbcab3a54e
StratulatStefan <stefanc.stratulat@gmail.com>
2020-07-15 22:57:24 -0900

    handshake-refactorization

--- ConnectionEstablish/receiver/general_use.py
+++ ConnectionEstablish/receiver/general_use.py
@@ -11,6 +11,61 @@ communication_type = lambda message : 0 if "TCP" in message else 1 if "UART" in 
 
 # functie lambda care returneaza continutul necesar din mesajul specific Handshake-ului
 relevant_data_handshake = lambda message : re.findall("\[HS\]\s(.+)", message)[0]
+
 # functie lambda care extrage un numar dintr-un string
 # folosita in cazul extragerii index-ului din ACK
 extract_number = lambda message : re.findall("[0-9]+", message)[0]
+
+
+# functie folosita pentru transmiterea octetilor prin socket TCP
+def socketWRITE(sock, message):
+        # trimitem mesajul encodat
+	sock.sendall(message)
+
+# functie folosita pentru transmiterea unui mesaj prin socket TCP
+def socketWRITEMessage(sock, message):
+        # encodam messajul
+	encoded_msg = data_encode(message)
+	socketWRITE(sock, encoded_msg)
+
+# functie folosita pentru transmiterea octetilor prin UART
+def uartWRITE(port, message):
+        # trimitem mesajul encodat
+	port.write(message)
+
+# functie folosita pentru transmiterea unui mesaj prin UART
+def uartWRITEMessage(port, message):
+        # encodam mesajul
+	encoded_msg = data_encode(message)
+        # trimitem mesajul encodat
+	uartWRITE(port, encoded_msg)
+
+# functie pentru citirea octetilor din socket-ul TCP
+def socketREAD(sock):
+        # citim si returnam 1024 de octeti din socket
+	return sock.recv(1024)
+
+# functie pentru citirea unui mesaj de pe socket-ul TCP
+def socketREADMessage(sock):
+        # citim octetii din socket
+	message = socketREAD(sock)
+        # decodam si returnam octetii
+	return data_decode(message)
+
+# functie pentru citirea octetilor din UART
+def uartREAD(port):
+        # citim 1 octet de pe portul serial
+	data = port.read()
+        # determinam numarul de octeti ramasi in bufferul de intrare
+	remaining_bytes = port.inWaiting()
+        # citim octetii ramasi
+	data += port.read(remaining_bytes)
+	return data
+
+# functie pentru citirea unui mesaj din UART
+def uartREADMessage(port):
+        # citim octetii din UART
+	data = uartREAD(port)
+        # decodam si returnam mesajul
+	return data_decode(data)
+
--- ConnectionEstablish/receiver/handshake.py
+++ ConnectionEstablish/receiver/handshake.py
@@ -23,91 +23,54 @@ def CommunicationHandshake(sock, data) -> bool:
 
 	# incepem secventa de trimitere de mesaje de verificare pe canalul de comunicare stabilit
 	print(f"* Incepem secventa de verificare specifica {comtypeName}...")
+	connection = None
 	if comtypeId == 0 :
-		if not TCPConnectionCheck(sock):
-			return ErrorResponse
-		return 0, sock, True
+		connection = sock
 	elif comtypeId == 1:
-		UARTConnection = serial.Serial("/dev/ttyS0", baudrate = 576000, timeout=None)
-		if not UARTConnectionCheck(UARTConnection):
-			return ErrorResponse
-		return 1, UARTConnection, True
-	else:
+		connection = serial.Serial("/dev/ttyS0", baudrate = 576000, timeout=None)
+
+	invalidcomtypeId = comtypeId < 0 or comtypeId > 1
+	if invalidcomtypeId or not ConnectionCheck(connection, comtypeId):
 		return ErrorResponse
+
+	return comtypeId, connection, True
 	print("******************\n\n")
 
-def UARTConnectionCheck(port) -> bool:
+# functia care trimite mesaje de verificare a conexiunii, corespunzator mediului de comunicare ales
+def ConnectionCheck(connection, selection) -> bool:
+	# determinam functiile necesare pentru citire si scriere, pe baza tipul de comunicare ales
+	# folosim referinte la acele functii
+	readFunction = uartREADMessage if selection == 1 else socketREADMessage if selection == 0 else None
+	writeFunction = uartWRITEMessage if selection == 1 else socketWRITEMessage if selection == 0 else None
+
 	print("\n\n")
 	# ne asteptam sa primim 3 mesaje
 	messages_count = 1
 	while True:
 		# asteptam primirea unui mesaj
 		print("* Asteptam mesajul...")
-		data = port.read()
-		remaining_bytes = port.inWaiting()
-		data += port.read(remaining_bytes)
-		# am primit un mesaj, pe care il decodificam
-		decoded_data = data_decode(data)
+		data = readFunction(connection)
 		# extragem doar continutul relevant
-		if "[HS]" not in decoded_data:
+		if "[HS]" not in data:
 			pass
-		decoded_data = relevant_data_handshake(decoded_data)
+		decoded_data = relevant_data_handshake(data)
 		print(f">>> Am primit mesajul {decoded_data}")
-        # extragem indexul verificarii din mesaj
+ 		# extragem indexul verificarii din mesaj
 		id = int(extract_number(decoded_data))
 		if id > messages_count:
 			break
-#              pass
+		#	pass
 		elif id == messages_count:
 			messages_count += 1
-            # am primit mesajul corect
-            # compunem mesajul de confirmare
+            		# am primit mesajul corect
+            		# compunem mesajul de confirmare
 			ack = f"[HS] ACK MSG{id}"
-            # encodam mesajul de confirmare
-			encoded_ack = data_encode(ack)
-            # trimitem confirmare
+            		# trimitem confirmare
 			print(f"* Trimitem mesajul de confirmare pentru MSG{id}")
-			port.write(encoded_ack)
-			print(f">>> Am trimis confirmare pentru MSG{id}!")
-			if id == 3:
-				break   
-	print(f"* Handshake realizat cu succes!")
-	return True
-
-
-# functia care trimite mesaje de verificare a conexiunii prin Socket-uri TCP
-def TCPConnectionCheck(sock) -> bool:
-	print("\n\n")
-	# ne asteptam sa primim 3 mesaje
-	messages_count = 1
-	while True:
-		# asteptam primirea unui mesaj
-		print("* Asteptam mesajul...")
-		data = sock.recv(1024)
-		# am primit un mesaj, pe care il decodificam
-		decoded_data = data_decode(data)
-		# extragem doar continutul relevant
-		if "[HS]" not in decoded_data:
-			pass
-		decoded_data = relevant_data_handshake(decoded_data)
-		print(f">>> Am primit mesajul {decoded_data}")
-		# extragem indexul verificarii din mesaj
-		id = int(extract_number(decoded_data))
-		if id > messages_count:
-			break
-#			pass
-		elif id == messages_count:
-			messages_count += 1
-			# am primit mesajul corect
-			# compunem mesajul de confirmare
-			ack = f"[HS] ACK MSG{id}"
-			# encodam mesajul de confirmare
-			encoded_ack = data_encode(ack)
-			# trimitem confirmare
-			print(f"* Trimitem mesajul de confirmare pentru MSG{id}")
-			sock.sendall(encoded_ack)
+			writeFunction(connection, ack)
 			print(f">>> Am trimis confirmare pentru MSG{id}!")
 			if id == 3:
 				break
 	print(f"* Handshake realizat cu succes!")
 	return True
+
