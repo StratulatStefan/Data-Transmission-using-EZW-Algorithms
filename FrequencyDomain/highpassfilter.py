bc57c7f6f97cce80144c31421b74c872ff16c0ad
Stratulat Stefan <stefanc.stratulat@gmail.com>
2020-07-23 12:53:58 +0300

    some-more-freq-filters

--- FrequencyDomain/highpassfilter.py
+++ FrequencyDomain/highpassfilter.py
@@ -3,16 +3,89 @@ from api.fourier import *
 from api.plotter import *
 from api.general_use import *
 
-# functie care creeaza un filtru trece-sus
+# functie care creeaza un filtru trece-sus patract
 # size  : dimensiunea filtrului (ex : 512,512)
 # level : frecventa de taiere
-def HighPassFilter(size, level):
+def HighPassFilterSquare(size, level):
     rows, cols = size
     crow, ccol = int(rows / 2), int(cols / 2)
 
     mask = np.ones((rows, cols, 2), np.uint8)
     mask[crow - level:crow + level, ccol - level:ccol + level] = 0
     return mask
+
+###############################################################################
+
+# functie care creeaza un filtru trece-sus ideal
+# size  : dimensiunea filtrului (ex : 512,512)
+# level : frecventa de taiere
+def HighPassFilterIdeal(size, level):
+    rows, cols = size
+    crow, ccol = int(rows / 2), int(cols / 2)
+
+    mask = np.ones((rows, cols, 2), np.uint8)
+
+    # - restrangem cautarea la un patrat de latura = level, deoarece incadreaza perfect cercul
+    # eliminam astfel parcurgerea unor valori fara sens
+    for cr in range(crow - level, crow + level):
+        for cl in range(ccol - level, ccol + level):
+            distance = np.sqrt(np.power(cr - crow, 2) + np.power(cl - ccol,2))
+            if distance < level:
+                mask[cr, cl] = 0
+
+    return mask
+
+###############################################################################
+
+# functie care creeaza un filtru trece-sus Butterworth
+# size  : dimensiunea filtrului
+# level : frecventa de taiere
+# grade : gradul filtrului
+def HighPassFilterButterWorth(size, level, grade):
+    rows, cols = size
+    crow, ccol = int(rows / 2), int(cols / 2)
+
+    mask = np.zeros((rows, cols, 2), np.uint8)
+
+
+    # valorile filtrului trebuie sa fie valori reale, avand in vedere ca
+    # ecuatia filtrului contine o impartire
+    mask = np.float32(mask)
+
+    # valorile filtrului trebuie sa fie valori reale, avand in vedere ca
+    # ecuatia filtrului contine o impartire
+    mask = np.float32(mask)
+
+    for cr in range(rows):
+        for cl in range(cols):
+            distance = np.sqrt(np.power(cr - crow, 2) + np.power(cl - ccol,2))
+            if distance != 0:
+                mask[cr, cl] = np.power(1 + np.power(level / distance, 2 * grade),-1)
+
+    return mask
+
+###############################################################################
+
+# functie care creeaza un filtru trece-sus Gaussian
+# size  : dimensiunea filtrului
+# level : frecventa de taiere
+def HighPassFilterGaussian(size, level):
+    rows, cols = size
+    crow, ccol = int(rows / 2), int(cols / 2)
+
+    mask = np.zeros((rows, cols, 2), np.uint8)
+
+    # valorile filtrului trebuie sa fie valori reale, avand in vedere ca
+    # ecuatia filtrului contine o impartire
+    mask = np.float32(mask)
+
+    for cr in range(rows):
+        for cl in range(cols):
+            distance = np.sqrt(np.power(cr - crow, 2) + np.power(cl - ccol,2))
+            mask[cr, cl] = 1 - np.exp(-np.power(distance,2) / (2 * np.power(level,2)))
+
+    return mask
+
 
 if __name__ == "__main__":
     imagePATH = "D:\Confidential\EZW Algorithm\lena.png"
@@ -25,10 +98,33 @@ if __name__ == "__main__":
         exit(-1)
 
     #####################################################
-    lowpass15 = HighPassFilter(image.shape, 15)
-    lowpass30 = HighPassFilter(image.shape, 30)
-    lowpass50 = HighPassFilter(image.shape, 50)
-    lowpass65 = HighPassFilter(image.shape, 65)
+
+    lowpass15 = HighPassFilterGaussian(image.shape, 15)
+    lowpass30 = HighPassFilterGaussian(image.shape, 30)
+    lowpass50 = HighPassFilterGaussian(image.shape, 50)
+    lowpass65 = HighPassFilterGaussian(image.shape,
+                                       65)
+
+    '''
+    lowpass15 = HighPassFilterButterWorth(image.shape, 15, 2)
+    lowpass30 = HighPassFilterButterWorth(image.shape, 30, 2)
+    lowpass50 = HighPassFilterButterWorth(image.shape, 50, 2)
+    lowpass65 = HighPassFilterButterWorth(image.shape, 65, 2)
+    '''
+
+    '''
+    lowpass15 = HighPassFilterIdeal(image.shape, 15)
+    lowpass30 = HighPassFilterIdeal(image.shape, 30)
+    lowpass50 = HighPassFilterIdeal(image.shape, 50)
+    lowpass65 = HighPassFilterIdeal(image.shape, 65)
+    '''
+    '''
+    lowpass15 = HighPassFilterSquare(image.shape, 15)
+    lowpass30 = HighPassFilterSquare(image.shape, 30)
+    lowpass50 = HighPassFilterSquare(image.shape, 50)
+    lowpass65 = HighPassFilterSquare(image.shape, 65)
+    '''
+
     #####################################################
     fourier = Fourier(image)
     fourier15 = fourier * lowpass15
--- FrequencyDomain/lowpassfilter.py
+++ FrequencyDomain/lowpassfilter.py
@@ -2,15 +2,81 @@ from api.plotter import *
 from api.fourier import *
 from api.general_use import *
 
-# functie care creeaza un filtru trece-jos
+# functie care creeaza un filtru trece-jos patrat
 # size  : dimensiunea filtrului (ex : 512,512)
 # level : frecventa de taiere
-def LowPassFilter(size, level):
+def LowPassFilterSquare(size, level):
     rows, cols = size
     crow, ccol = int(rows / 2), int(cols / 2)
 
     mask = np.zeros((rows, cols, 2), np.uint8)
     mask[crow - level:crow + level, ccol - level:ccol + level] = 1
+    return mask
+
+###############################################################################
+
+# functie care creeaza un filtru trece-jos ideal
+# size  : dimensiunea filtrului (ex : 512,512)
+# level : frecventa de taiere
+def LowPassFilterIdeal(size, level):
+    rows, cols = size
+    crow, ccol = int(rows / 2), int(cols / 2)
+
+    mask = np.zeros((rows, cols, 2), np.uint8)
+
+    # - restrangem cautarea la un patrat de latura = level, deoarece incadreaza perfect cercul
+    # eliminam astfel parcurgerea unor valori fara sens
+    for cr in range(crow - level, crow + level):
+        for cl in range(ccol - level, ccol + level):
+            distance = np.sqrt(np.power(cr - crow, 2) + np.power(cl - ccol,2))
+            if distance < level:
+                mask[cr, cl] = 1
+
+    return mask
+
+###############################################################################
+
+# functie care creeaza un filtru trece-jos Butterworth
+# size  : dimensiunea filtrului
+# level : frecventa de taiere
+# grade : gradul filtrului
+def LowPassFilterButterWorth(size, level, grade):
+    rows, cols = size
+    crow, ccol = int(rows / 2), int(cols / 2)
+
+    mask = np.zeros((rows, cols, 2), np.uint8)
+
+    # valorile filtrului trebuie sa fie valori reale, avand in vedere ca
+    # ecuatia filtrului contine o impartire
+    mask = np.float32(mask)
+
+    for cr in range(rows):
+        for cl in range(cols):
+            distance = np.sqrt(np.power(cr - crow, 2) + np.power(cl - ccol,2))
+            mask[cr, cl] = np.power(1 + np.power(distance / level,2 * grade),-1)
+
+    return mask
+
+###############################################################################
+
+# functie care creeaza un filtru trece-jos Gaussian
+# size  : dimensiunea filtrului
+# level : frecventa de taiere
+def LowPassFilterGaussian(size, level):
+    rows, cols = size
+    crow, ccol = int(rows / 2), int(cols / 2)
+
+    mask = np.zeros((rows, cols, 2), np.uint8)
+
+    # valorile filtrului trebuie sa fie valori reale, avand in vedere ca
+    # ecuatia filtrului contine o impartire
+    mask = np.float32(mask)
+
+    for cr in range(rows):
+        for cl in range(cols):
+            distance = np.sqrt(np.power(cr - crow, 2) + np.power(cl - ccol,2))
+            mask[cr, cl] = np.exp(-np.power(distance,2) / (2 * np.power(level,2)))
+
     return mask
 
 if __name__ == "__main__":
@@ -24,10 +90,31 @@ if __name__ == "__main__":
         exit(-1)
 
     #############################################
-    lowpass15 = LowPassFilter(image.shape, 15)
-    lowpass30 = LowPassFilter(image.shape, 30)
-    lowpass50 = LowPassFilter(image.shape, 50)
-    lowpass65 = LowPassFilter(image.shape, 65)
+
+    lowpass15 = LowPassFilterGaussian(image.shape, 15)
+    lowpass30 = LowPassFilterGaussian(image.shape, 30)
+    lowpass50 = LowPassFilterGaussian(image.shape, 50)
+    lowpass65 = LowPassFilterGaussian(image.shape, 65)
+
+    '''
+    lowpass15 = LowPassFilterButterWorth(image.shape, 15, 2)
+    lowpass30 = LowPassFilterButterWorth(image.shape, 30, 2)
+    lowpass50 = LowPassFilterButterWorth(image.shape, 50, 2)
+    lowpass65 = LowPassFilterButterWorth(image.shape, 65, 2)
+    '''
+
+    '''
+    lowpass15 = LowPassFilterIdeal(image.shape, 15)
+    lowpass30 = LowPassFilterIdeal(image.shape, 30)
+    lowpass50 = LowPassFilterIdeal(image.shape, 50)
+    lowpass65 = LowPassFilterIdeal(image.shape, 165)
+    '''
+    '''
+    lowpass15 = LowPassFilterSquare(image.shape, 15)
+    lowpass30 = LowPassFilterSquare(image.shape, 30)
+    lowpass50 = LowPassFilterSquare(image.shape, 50)
+    lowpass65 = LowPassFilterSquare(image.shape, 65)
+    '''
     #############################################
     fourier = Fourier(image)
     fourier15 = fourier * lowpass15
